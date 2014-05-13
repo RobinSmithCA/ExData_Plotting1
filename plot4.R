998e2611c3a8ebfc173618416d53772f5879b58e
RobinSmithCA <robin@computingdreams.com>
2014-05-12 18:04:47 -0700

    Remove Kilowatts from yLabel of plot 1 and legend sizing in 3
    Minor tweak to y label and legend sizing

--- plot4.R
+++ plot4.R
@@ -11,14 +11,14 @@ openTarget( targetFile )
 # this will be a 2 x 2 with 4 total graphs.
 par( mfrow = c( 2, 2 ) )
 
-plot( rawData$TimeStamp , rawData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)" )
+plot( rawData$TimeStamp , rawData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power" )
 
 plot( rawData$TimeStamp , rawData$Voltage, type = "l", xlab = "datetime", ylab = "Voltage" )
 
 plot( rawData$TimeStamp , rawData$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering" )
 points( rawData$TimeStamp , rawData$Sub_metering_2, type = "l", col = "red" )
 points( rawData$TimeStamp , rawData$Sub_metering_3, type = "l", col = "blue" )
-legend( "topright", cex = .9, bty = "n", lwd = 2.0, col = c( "black", "red", "blue" ), legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ) )
+legend( "topright", cex = .95, bty = "n", lwd = 1.0, col = c( "black", "red", "blue" ), legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ) )
 
 plot( rawData$TimeStamp , rawData$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power" )
 
